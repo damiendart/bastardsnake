03686f33956732806f4240d58df78ca521fa12ed
Damien Dart <damiendart@pobox.com>
2013-08-22 22:28:31 +0100

    Add basic game-loop stuff.

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,9 +1,39 @@
 package;
 
 import flash.display.Sprite;
+import flash.events.Event;
+import flash.events.KeyboardEvent;
+import flash.Lib.getTimer;
 
 class Main extends Sprite {
+  private var _accumulated_time:Int;
+  private var _current_time:Int;
+
   public function new() {
     super();
+    this.addEventListener(Event.ADDED_TO_STAGE, this._onAddedToStage);
+  }
+
+  private function _onAddedToStage(event:Event):Void {
+    this._accumulated_time = 0;
+    this._current_time = getTimer();
+    stage.addEventListener(KeyboardEvent.KEY_DOWN, this._onKeyDown);
+    this.addEventListener(Event.ENTER_FRAME, this._onEnterFrame);
+  }
+
+  private function _onEnterFrame(event:Event):Void {
+    var new_time:Int = getTimer();
+    var dt:Int = new_time - _current_time;
+    this._current_time = new_time;
+    this._accumulated_time += (dt > 100) ? 100 : dt;
+    while (this._accumulated_time >= 10) {
+      /* ... */
+      this._accumulated_time -= 10;
+    }
+    /* ... */
+  }
+
+  private function _onKeyDown(event:KeyboardEvent):Void {
+    /* ... */
   }
 }
