a1dca27e1dc7841e7a708538f2131d58c6e9ee64
Damien Dart <damiendart@pobox.com>
2015-01-21 11:39:48 +0000

    Add licensing bumf.

--- Source/BasicGameStateManager.hx
+++ Source/BasicGameStateManager.hx
@@ -1,3 +1,8 @@
+// Copyright (C) 2013-2015 Damien Dart, <damiendart@pobox.com>.
+// This file is distributed under the MIT licence. For more information,
+// please refer to the accompanying "LICENCE" file.
+
+
 package;
 
 
